06e39d713bcf203619a36c4c7cfdc4df6b286a2a
Burrit-o <sameorio@gmail.com>
2025-01-14 19:28:27 -0700

    Add Semi-Working Pathfinding

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,11 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -15,6 +19,8 @@ import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.commands.SwerveJoystickCmd;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathConstraints;
+import com.pathplanner.lib.util.PathPlannerLogging;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -53,6 +59,12 @@ public class RobotContainer {
 
         // Build an auto chooser. This will use Commands.none() as the default option.
         autoChooser = AutoBuilder.buildAutoChooser();
+
+        SmartDashboard.putData("Pathfind to whoknoweswhere", AutoBuilder.pathfindToPose(
+          new Pose2d(1, 8, Rotation2d.fromDegrees(0)), 
+          new PathConstraints(.75, 1.0, 
+          Units.degreesToRadians(180), Units.degreesToRadians(270)), 
+          .5));
     }
 
     /**
