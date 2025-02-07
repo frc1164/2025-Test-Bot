a22c3eddd0ef2635e9d437a43efa85ea28bd335d
Burrit-o <sameorio@gmail.com>
2025-02-06 18:41:55 -0700

    First Commit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -16,16 +18,31 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
+  
   private final RobotContainer m_robotContainer;
+  private final LEDPattern m_rainbow = LEDPattern.rainbow(255, 128);
 
+  // Our LED strip has a density of 120 LEDs per meter
+  private static final Distance kLedSpacing = Meters.of(1 / 64.0);
+
+  // Create a new pattern that scrolls the rainbow pattern across the LED strip, moving at a speed
+  // of 1 meter per second.
+  private final LEDPattern m_scrollingRainbow =
+      m_rainbow.scrollAtAbsoluteSpeed(MetersPerSecond.of(.5), kLedSpacing);
   /**
-   * This function is run when the robot is first started up and should be used for any
+  * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   public Robot() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    
+   
+    // PWM port 9
+    // Must be a PWM header, not MXP or DIO
+    m_led = new AddressableLED(9);
+   
   }
 
   /**
@@ -98,4 +115,5 @@ public class Robot extends TimedRobot {
   /** This function is called periodically whilst in simulation. */
   @Override
   public void simulationPeriodic() {}
+
 }
---
+++ src/main/java/frc/robot/commands/LEDS.java
@@ -0,0 +1,53 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.LEDSubsystem;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class LEDS extends Command {
+  private final CommandXboxController m_CommandXboxController;
+  private final LEDSubsystem m_LedSubsystem;
+  
+  /** Creates a new LEDS. */
+  
+  public LEDS(LEDSubsystem ledSubsystem, CommandXboxController controller) {
+    m_LedSubsystem = ledSubsystem; 
+    m_CommandXboxController = controller;
+
+    // Use addRequirements() here to declare subsystem dependencies.
+
+    addRequirements(m_LedSubsystem);
+  
+  }
+
+ 
+ 
+ 
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    LEDPattern pattern = LEDPattern.progressMaskLayer(() -> m_CommandXboxController.getRightTriggerAxis() / 1);
+
+    m_LedSubsystem.runPattern(pattern);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
