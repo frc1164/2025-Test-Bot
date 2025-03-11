ac3fd454d51d88c1c89ef2d0e919cfd8912df7f9
Sickmantella <radioqacker@duck.com>
2025-03-11 15:14:22 -0600

    Fix constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,6 +28,7 @@ public final class Constants {
     public static class OperatorConstants {
         public static final int kDriverControllerPort = 0;
         public static final int kOperatorControllerPort = 1;
+        public static final int ToFID = 50;
 
         public static final double kDeadband = 0.25;
   }
