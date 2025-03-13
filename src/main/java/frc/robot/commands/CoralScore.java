7076f84c22f76f0b91e3bdf7b295ac5dd83d177d
Sickmantella <radioqacker@duck.com>
2025-03-13 16:03:06 -0600

    Untested - Maybe add red alliance to coral score logic. Add paths/autos from auto branch.

--- src/main/java/frc/robot/commands/CoralScore.java
+++ src/main/java/frc/robot/commands/CoralScore.java
@@ -33,6 +33,8 @@ public class CoralScore extends Command {
   //private EventMarker ledsOn, ledsOff;
   private boolean left;
   private LEDSubsystem m_LedSubsystem;
+  private int principalTag;
+  private boolean flipPath;
   // private double happy;
   // private double poseDiff;
 
@@ -40,6 +42,7 @@ public class CoralScore extends Command {
     subsystem = m_subsystem;
     left = isLeft;
     m_LedSubsystem = ledSubsystem;
+    flipPath = false; // Default to blue side, unless changed. Prevent crashes associated with null.
 
   }
 
@@ -54,79 +57,123 @@ public class CoralScore extends Command {
     // GH = 21
     // IJ = 20
     // KL = 19
-    if (left) {
-      switch (subsystem.getPrincipalTag()) {
-        case 17:
-          targetPose = new Pose2d(3.965, 2.95, new Rotation2d(Math.PI / 3));
-          break;
-        case 18:
-          targetPose = new Pose2d(3.165, 3.93, new Rotation2d(0));
-          break;
-        case 19:
-          targetPose = new Pose2d(2.325, 3.72, new Rotation2d(Math.PI * 5 / 3));
-          break;
-        case 20:
-          targetPose = new Pose2d(5.01, 5.265, new Rotation2d(Math.PI * 4 / 3));
-          break;
-        case 21:
-          targetPose = new Pose2d(5.825, 4.16, new Rotation2d(Math.PI));
-          break;
-        case 22:
-          targetPose = new Pose2d(5.29, 2.95, new Rotation2d(Math.PI * 2 / 3));
-          break;
-        default:
-          targetPose = null;
-          break;
+
+    principalTag = subsystem.getPrincipalTag();
+
+    // Ensure there is a principle tag.
+    if (principalTag > 0) {
+      // The tag is on the RED alliance side, so the path needs to be flipped.
+      if (principalTag < 12) {
+        flipPath = true;
+        // Translate all the tags to be the field flipped twin tag.
+        switch (principalTag) {
+          case 6:
+            principalTag = 19;
+            break;
+          case 7:
+            principalTag = 18;
+            break;
+          case 8:
+            principalTag = 17;
+            break;
+          case 9:
+            principalTag = 22;
+            break;
+          case 10:
+            principalTag = 21;
+            break;
+          case 11:
+            principalTag = 20;
+            break;
+          default:
+            principalTag = 0;
+            break;
+        }
+      } else {
+        // The tag is on the BLUE alliance side, so no flipping is required.
+        flipPath = false;
       }
-    } else if (!left) {
-      switch (subsystem.getPrincipalTag()) {
-        case 17:
-          targetPose = new Pose2d(3.93, 2.82, new Rotation2d(Math.PI / 3));
-          break;
-        case 18:
-          targetPose = new Pose2d(3.22, 3.63, new Rotation2d(0));
-          break;
-        case 19:
-          targetPose = new Pose2d(3.66, 5.085, new Rotation2d(Math.PI * 5 / 3));
-          break;
-        case 20:
-          targetPose = new Pose2d(5.28, 5.12, new Rotation2d(Math.PI * 4 / 3));
-          break;
-        case 21:
-          targetPose = new Pose2d(5.825, 3.86, new Rotation2d(Math.PI));
-          break;
-        case 22:
-          targetPose = new Pose2d(5, 2.78, new Rotation2d(Math.PI * 2 / 3));
-          break;
-        default:
-          targetPose = null;
-          break;
+
+      // Left side coordinates.
+      if (left) {
+        // Translate all tags to coordinates. Flipped below if needed.
+        switch (principalTag) {
+          case 17:
+            targetPose = new Pose2d(3.965, 2.95, new Rotation2d(Math.PI / 3));
+            break;
+          case 18:
+            targetPose = new Pose2d(3.165, 3.93, new Rotation2d(0));
+            break;
+          case 19:
+            targetPose = new Pose2d(2.325, 3.72, new Rotation2d(Math.PI * 5 / 3));
+            break;
+          case 20:
+            targetPose = new Pose2d(5.01, 5.265, new Rotation2d(Math.PI * 4 / 3));
+            break;
+          case 21:
+            targetPose = new Pose2d(5.825, 4.16, new Rotation2d(Math.PI));
+            break;
+          case 22:
+            targetPose = new Pose2d(5.29, 2.95, new Rotation2d(Math.PI * 2 / 3));
+            break;
+          default:
+            targetPose = null;
+            break;
+        }
+      } else if (!left) { // Right side coordinates.
+        switch (principalTag) {
+          case 17:
+            targetPose = new Pose2d(3.93, 2.82, new Rotation2d(Math.PI / 3));
+            break;
+          case 18:
+            targetPose = new Pose2d(3.22, 3.63, new Rotation2d(0));
+            break;
+          case 19:
+            targetPose = new Pose2d(3.66, 5.085, new Rotation2d(Math.PI * 5 / 3));
+            break;
+          case 20:
+            targetPose = new Pose2d(5.28, 5.12, new Rotation2d(Math.PI * 4 / 3));
+            break;
+          case 21:
+            targetPose = new Pose2d(5.825, 3.86, new Rotation2d(Math.PI));
+            break;
+          case 22:
+            targetPose = new Pose2d(5, 2.78, new Rotation2d(Math.PI * 2 / 3));
+            break;
+          default:
+            targetPose = null;
+            break;
+        }
+      }
+      currentPose = subsystem.getPose();
+      if (targetPose != null) {
+        if (!targetPose.equals(currentPose)) {
+          waypoints = PathPlannerPath.waypointsFromPoses(currentPose, targetPose);
+          constraints = new PathConstraints(3, 1.5, 1, .5);
+          endstate = new GoalEndState(0, targetPose.getRotation());
+
+          // ledsOn = new EventMarker("leds", 0);
+          // ledsOn.command().alongWith(
+          // new InstantCommand(() ->
+          // m_LedSubsystem.setPattern3(m_LedSubsystem.colorWhite())),
+          // new InstantCommand(() ->
+          // m_LedSubsystem.setPattern4(m_LedSubsystem.colorWhite())));
+
+          // ledsOff = new EventMarker("leds", 1);
+          // ledsOff.command().alongWith(
+          // new InstantCommand(() ->
+          // m_LedSubsystem.setPattern3(m_LedSubsystem.colorPurple())),
+          // new InstantCommand(() ->
+          // m_LedSubsystem.setPattern4(m_LedSubsystem.colorOrange())));
+
+          path = new PathPlannerPath(waypoints, constraints, null, endstate);
+          path.preventFlipping = flipPath; // Flip path if the original tag was on the RED alliance side.
+          subsystem.currentPath = AutoBuilder.followPath(path);
+          subsystem.currentPath.schedule();
+        }
       }
     }
-    currentPose = subsystem.getPose();
-    if (targetPose != null) {
-      if (!targetPose.equals(currentPose)) {
-        waypoints = PathPlannerPath.waypointsFromPoses(currentPose, targetPose);
-        constraints = new PathConstraints(3, 1.5, 1, .5);
-        endstate = new GoalEndState(0, targetPose.getRotation());
-
-        // ledsOn = new EventMarker("leds", 0);
-        // ledsOn.command().alongWith(
-        //   new InstantCommand(() -> m_LedSubsystem.setPattern3(m_LedSubsystem.colorWhite())),
-        //   new InstantCommand(() -> m_LedSubsystem.setPattern4(m_LedSubsystem.colorWhite())));
-
-        // ledsOff = new EventMarker("leds", 1);
-        // ledsOff.command().alongWith(
-        //   new InstantCommand(() -> m_LedSubsystem.setPattern3(m_LedSubsystem.colorPurple())),
-        //   new InstantCommand(() -> m_LedSubsystem.setPattern4(m_LedSubsystem.colorOrange())));  
-
-        path = new PathPlannerPath(waypoints, constraints, null, endstate);
-      
-        subsystem.currentPath = AutoBuilder.followPath(path);
-        subsystem.currentPath.schedule();
-      }
-    }
-    SmartDashboard.putNumber("seen", subsystem.getPrincipalTag());
+    SmartDashboard.putNumber("seen", principalTag);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -148,3 +195,167 @@ public class CoralScore extends Command {
     return true;
   }
 }
+
+// ORIGINAL FILE BEFORE WEIRD LOGIC WAS INTRODUCED.
+/*
+ * // Copyright (c) FIRST and other WPILib contributors.
+ * // Open Source Software; you can modify and/or share it under the terms of
+ * // the WPILib BSD license file in the root directory of this project.
+ * 
+ * package frc.robot.commands;
+ * 
+ * import java.util.List;
+ * 
+ * import com.pathplanner.lib.auto.AutoBuilder;
+ * import com.pathplanner.lib.path.EventMarker;
+ * import com.pathplanner.lib.path.GoalEndState;
+ * import com.pathplanner.lib.path.PathConstraints;
+ * import com.pathplanner.lib.path.PathPlannerPath;
+ * import com.pathplanner.lib.path.Waypoint;
+ * 
+ * import edu.wpi.first.math.geometry.Pose2d;
+ * import edu.wpi.first.math.geometry.Rotation2d;
+ * import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+ * import edu.wpi.first.wpilibj2.command.Command;
+ * import edu.wpi.first.wpilibj2.command.CommandScheduler;
+ * import edu.wpi.first.wpilibj2.command.InstantCommand;
+ * import frc.robot.subsystems.LEDSubsystem;
+ * import frc.robot.subsystems.SwerveSubsystem;
+ * 
+ * public class CoralScore extends Command {
+ * private final SwerveSubsystem subsystem;
+ * private List<Waypoint> waypoints;
+ * private PathConstraints constraints;
+ * private Pose2d targetPose;
+ * private Pose2d currentPose;
+ * private PathPlannerPath path;
+ * private GoalEndState endstate;
+ * private EventMarker ledsOn, ledsOff;
+ * private boolean left;
+ * private LEDSubsystem m_LedSubsystem;
+ * // private double happy;
+ * // private double poseDiff;
+ * 
+ * public CoralScore(SwerveSubsystem m_subsystem, boolean isLeft, LEDSubsystem
+ * ledSubsystem) {
+ * subsystem = m_subsystem;
+ * left = isLeft;
+ * m_LedSubsystem = ledSubsystem;
+ * 
+ * }
+ * 
+ * @Override
+ * public void initialize() {
+ * if (subsystem.currentPath != null) {
+ * CommandScheduler.getInstance().cancel(subsystem.currentPath);
+ * }
+ * // AB = 18
+ * // CD = 17
+ * // EF = 22
+ * // GH = 21
+ * // IJ = 20
+ * // KL = 19
+ * if (left) {
+ * switch (subsystem.getPrincipalTag()) {
+ * case 17:
+ * targetPose = new Pose2d(3.965, 2.95, new Rotation2d(Math.PI / 3));
+ * break;
+ * case 18:
+ * targetPose = new Pose2d(3.165, 3.93, new Rotation2d(0));
+ * break;
+ * case 19:
+ * targetPose = new Pose2d(2.325, 3.72, new Rotation2d(Math.PI * 5 / 3));
+ * break;
+ * case 20:
+ * targetPose = new Pose2d(5.01, 5.265, new Rotation2d(Math.PI * 4 / 3));
+ * break;
+ * case 21:
+ * targetPose = new Pose2d(5.825, 4.16, new Rotation2d(Math.PI));
+ * break;
+ * case 22:
+ * targetPose = new Pose2d(5.29, 2.95, new Rotation2d(Math.PI * 2 / 3));
+ * break;
+ * default:
+ * targetPose = null;
+ * break;
+ * }
+ * } else if (!left) {
+ * switch (subsystem.getPrincipalTag()) {
+ * case 17:
+ * targetPose = new Pose2d(3.93, 2.82, new Rotation2d(Math.PI / 3));
+ * break;
+ * case 18:
+ * targetPose = new Pose2d(3.22, 3.63, new Rotation2d(0));
+ * break;
+ * case 19:
+ * targetPose = new Pose2d(3.66, 5.085, new Rotation2d(Math.PI * 5 / 3));
+ * break;
+ * case 20:
+ * targetPose = new Pose2d(5.28, 5.12, new Rotation2d(Math.PI * 4 / 3));
+ * break;
+ * case 21:
+ * targetPose = new Pose2d(5.825, 3.86, new Rotation2d(Math.PI));
+ * break;
+ * case 22:
+ * targetPose = new Pose2d(5, 2.78, new Rotation2d(Math.PI * 2 / 3));
+ * break;
+ * default:
+ * targetPose = null;
+ * break;
+ * }
+ * }
+ * currentPose = subsystem.getPose();
+ * if (targetPose != null) {
+ * if (!targetPose.equals(currentPose)) {
+ * waypoints = PathPlannerPath.waypointsFromPoses(currentPose, targetPose);
+ * constraints = new PathConstraints(3, 1.5, 1, .5);
+ * endstate = new GoalEndState(0, targetPose.getRotation());
+ * 
+ * // ledsOn = new EventMarker("leds", 0);
+ * // ledsOn.command().alongWith(
+ * // new InstantCommand(() ->
+ * m_LedSubsystem.setPattern3(m_LedSubsystem.colorWhite())),
+ * // new InstantCommand(() ->
+ * m_LedSubsystem.setPattern4(m_LedSubsystem.colorWhite())));
+ * 
+ * // ledsOff = new EventMarker("leds", 1);
+ * // ledsOff.command().alongWith(
+ * // new InstantCommand(() ->
+ * m_LedSubsystem.setPattern3(m_LedSubsystem.colorPurple())),
+ * // new InstantCommand(() ->
+ * m_LedSubsystem.setPattern4(m_LedSubsystem.colorOrange())));
+ * 
+ * path = new PathPlannerPath(waypoints, constraints, null, endstate);
+ * 
+ * subsystem.currentPath = AutoBuilder.followPath(path);
+ * subsystem.currentPath.schedule();
+ * }
+ * }
+ * SmartDashboard.putNumber("seen", subsystem.getPrincipalTag());
+ * }
+ * 
+ * // Called every time the scheduler runs while the command is scheduled.
+ * 
+ * @Override
+ * public void execute() {
+ * // poseDiff = Math.sqrt(Math.pow(subsystem.getPose().getX() -
+ * targetPose.getX(),
+ * // 2) + Math.pow(currentPose.getY() - targetPose.getY(), 2));
+ * // SmartDashboard.putNumber("DistToTarget", poseDiff);
+ * }
+ * 
+ * // Called once the command ends or is interrupted.
+ * 
+ * @Override
+ * public void end(boolean interrupted) {
+ * }
+ * 
+ * // Returns true when the command should end.
+ * 
+ * @Override
+ * public boolean isFinished() {
+ * return true;
+ * }
+ * }
+ * 
+ */
